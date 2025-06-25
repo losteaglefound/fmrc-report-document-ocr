defbd975976fec7960d7f8de814d545c596084ea
losteaglefound <losteaglefound@gmail.com>
2025-06-25 17:56:44 +0530

    project packaging

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,6 +4,7 @@ from fastapi import (
 )
 from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 from .routers import api_router
 from ..common.config import setting
@@ -25,3 +26,10 @@ async def read_root():
         },
         status_code=status.HTTP_200_OK
     )
+
+
+def main():
+    uvicorn.run("backend.api.main:app", host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
